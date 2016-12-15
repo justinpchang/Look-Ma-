66a6927f6fe02128c7e4a05bca16b90a07044af8
Justin Chang <justin.p.chang@gmail.com>
2016-12-14 22:46:20 -0500

    Fully working focus(zone) method and rewrote leftClick() method to work properly

--- content.js
+++ content.js
@@ -2,13 +2,55 @@
  * Variables
  */
 var operatingLevel = 0; // Grid level (original is 0, once embedded is 1)
+var curX,curY; // Need to update curX and curY manually everytime operatingLevel changes
 
-// Initialize style
+/**
+ * Operation
+ */
+// Initialize style and draw main grid (oL0)
 styleInit();
 
 /**
  * Functions
  */
+// Focus on a zone and draw an embedded grid
+function focus(zone) {
+    var x = curX;
+    var y = curY;
+    var width = 100/Math.pow(3,operatingLevel);
+    var height = width;
+    // row
+    switch(Math.floor(zone/10)) {
+        case 1:
+            y += 0;
+            break;
+        case 2:
+            y += height;
+            break;
+        case 3:
+            y += 2 * height;
+            break;
+    }
+    // col
+    switch(zone%10) {
+        case 1:
+            x += 0;
+            break;
+        case 2:
+            x += width;
+            break;
+        case 3:
+            x += 2 * width;
+            break;
+    }
+    // Draw grid using calculated parameters
+    drawGrid(x, y, width, height);
+    console.log("\n\nfocus()");
+    console.log("x: " + x);
+    console.log("y: " + y);
+    console.log("width: " + width);
+    console.log("height: " + height);
+}
 
 // Click in the middle of a given zone
 /* Zones:
@@ -17,34 +59,39 @@ styleInit();
  * 31  32  33
  */
 function leftClick(zone) {
-    var x,y;
-    var width = $(window).width();
-    var height = $(window).height();
-    // x-coordinate of zone
+    var x = curX;
+    var y = curY;
+    var width = 100/Math.pow(3,operatingLevel-1);
+    var height = width;
+    // row
     switch(Math.floor(zone/10)) {
         case 1:
-            x = width/6;
+            y += height/6;
             break;
         case 2:
-            x = width/2;
+            y += height/2;
             break;
         case 3:
-            x = width*5/6;
+            y += height*5/6;
             break;
     }
-    // y-coordinate of zone
+    // col
     switch(zone%10) {
         case 1:
-            y = height/6;
+            x += width/6;
             break;
         case 2:
-            y = height/2;
+            x += width/2;
             break;
         case 3:
-            y = height*5/6;
+            x += width*5/6;
             break;
     }
+    // Convert percentage to pixels
+    x = $(window).width() * x/100;
+    y = $(window).height() * y/100;
     // Debug messages
+    console.log("\n\nleftClick()");
     console.log("width: " + width);
     console.log("height: " + height);
     console.log("zone: " + zone);
@@ -56,6 +103,7 @@ function leftClick(zone) {
 
 // Simulate clicks. Code taken from (http://stackoverflow.com/a/16509592)
 function simClick(x,y){
+    // Click at x and y (pixel value)
     var ev = document.createEvent("MouseEvent");
     var el = document.elementFromPoint(x,y);
     ev.initMouseEvent(
@@ -74,19 +122,21 @@ function styleInit() {
     $('body').append('\
     <style> \
     .vertical_line { \
-        width:3px; \
+        width:1px; \
         background-color:#F00; \
         position:fixed; \
         z-index:1000; \
     } \
     .horizontal_line { \
-        height:3px; \
+        height:1px; \
         background-color:#F00; \
         position:fixed; \
         z-index:1000; \
     } \
     </style> \
     ');
+    // Draw initial grid
+    drawGrid(0,0,100,100);
 }
 
 // Draw grid given top-left coordinates, width, and height (all in %)
@@ -119,16 +169,19 @@ function drawGrid(x, y, width, height) {
     } \n\
     #h' + operatingLevel + '1 { \n\
         top:' + (y + singleHeight) + '%; \n\
-        left:' + x + '; \n\
+        left:' + x + '%; \n\
         right:' + (100 - (x + width)) + '%; \n\
     } \n\
     #h' + operatingLevel + '2 { \n\
         top:' + (y + singleHeight * 2) + '%; \n\
-        left:' + x + '; \n\
+        left:' + x + '%; \n\
         right:' + (100 - (x + width)) + '%; \n\
     } \n\
     </style> \n\
     ');
-    console.log(code);
     $('body').append(code);
+    // Update global variables
+    curX = x;
+    curY = y;
+    operatingLevel++;
 }
